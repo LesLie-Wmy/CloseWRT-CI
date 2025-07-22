c003dca5eab6ab1fff0ab6e31be566ddf2e43836
LesLie-Wmy <133620294+LesLie-Wmy@users.noreply.github.com>
2025-07-22 10:44:44 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -94,3 +94,8 @@ if [ -d "$FRPC_DIR" ]; then
 
 	cd $PKG_PATH && echo "luci-app-frpc has been fixed!"
 fi
+
+#Update golang to 1.24
+echo "UPDATE golang to 1.24"
+rm -rf ../feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x ../feeds/packages/lang/golang
